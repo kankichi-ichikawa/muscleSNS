6f99e55f882e90f944f1eb91ef4cdee9a9f6f66b
kankichi-ichikawa <kankichi.ichikawa.2w@stu.hosei.ac.jp>
2021-11-10 11:35:40 +0900

    DB初期設定

---
+++ database/migrations/2021_11_10_014326_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id', 50);
+            $table->string('type', 50);
+            $table->string('title', 50);
+            $table->string('img', 50);
+            $table->string('body', 800);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
