2689271c2f5f266bd8e4c0be8b7447915840072b
kankichi-ichikawa <kankichi.ichikawa.2w@stu.hosei.ac.jp>
2021-12-07 14:08:10 +0900

    ログイン機能（装飾無）

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 require('./bootstrap');
+import Vue from 'vue'
+// ルーティングの定義をインポートする
+import router from './router'
+// ルートコンポーネントをインポートする
+import App from './App.vue'
+// ストア
+import store from './store'
+// CSRF対策
+import './bootstrap'
+
+const createApp = async () => {
+  await store.dispatch('auth/currentUser')
+
+new Vue({
+  el: '#app',
+  router, // ルーティングの定義を読み込む
+  store,
+  components: { App }, // ルートコンポーネントの使用を宣言する
+  template: '<App />' // ルートコンポーネントを描画する
+})
+}
+
+createApp()
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,26 +2,38 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name') }}</title>
+        
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+        
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https:/fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/app2.css') }}">
+        <link rel="stylesheet" href="{{asset('css/icon.min.css')}}">
+        
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        
     </head>
     <body>
-        <h1>Blog Name</h1>
-        <div class='posts'>
-            [<a href="/posts/create">create</a>]
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>
-                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                    </h2>
-                    <small>type: {{ $post->type }}</small>
-                    <p class='body'>{{ $post->body }}</p>
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
+        <div id="app"></div>
+        <!--  <h1>Index</h1>-->
+        <!--    <div class='posts'>-->
+        <!--    [<a href="/posts/create">create</a>]-->
+        <!--    @foreach ($posts as $post)-->
+        <!--        <div class='post'>-->
+        <!--            <h2 class='title'>-->
+        <!--                <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>-->
+        <!--            </h2>-->
+        <!--            <small>type: {{ $post->type }}</small>-->
+        <!--            <p class='body'>{{ $post->body }}</p>-->
+        <!--        </div>-->
+        <!--    @endforeach-->
+        <!--</div>-->
+        <!--<div class='paginate'>-->
+        <!--    {{ $posts->links() }}-->
+        <!--</div>-->
     </body>
 </html>
